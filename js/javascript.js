762c321406a6bf736388b1a542d67ecf0f8238bd
Grzegorz Baranski <gbaranski19@gmail.com>
2020-04-30 12:05:51 +0200

    fixed javascript errors, optimized code

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,6 @@
-
+$( document ).ready(function() {
+  getTimes();
+});
 window.onscroll = function() {shrinkNavbar()};
 function shrinkNavbar() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
@@ -10,36 +12,25 @@ function shrinkNavbar() {
   document.getElementById("logo-container").style.fontSize = "45px";
   document.getElementById("navbar").style.height = "90px";
   document.getElementById("navbar").style.lineHeight = "90px";
-
+  }
 }
-}
 
-var result;
+
 function getTimes() {
-    calcTimeDifference("2004-09-19");
-    document.getElementById("age").innerHTML = result;
-    calcTimeDifference("2020-01-12");
-    document.getElementById("googleCodeInDate").innerHTML = result;
-    calcTimeDifference("2020-01-13");
-    document.getElementById("waterMixerEspDate").innerHTML = result;
-    calcTimeDifference("2020-01-24");
-    document.getElementById("alarmClockEspDate").innerHTML = result;
-    calcTimeDifference("2020-03-01");
-    document.getElementById("downloadSpeedCalculatorDate").innerHTML = result;
-    calcTimeDifference("2020-03-04");
-    document.getElementById("olxBotDate").innerHTML = result;
-    calcTimeDifference("2020-03-10");
-    document.getElementById("desktopAlarmClockDate").innerHTML = result;
-
-
+    // document.getElementById("age").innerHTML = calcTimeDifference("2004-09-19");
+    document.getElementById("googleCodeInDate").innerHTML = calcTimeDifference("2020-01-12");
+    document.getElementById("waterMixerEspDate").innerHTML = calcTimeDifference("2020-01-13");
+    document.getElementById("alarmClockEspDate").innerHTML = calcTimeDifference("2020-01-24");
+    document.getElementById("downloadSpeedCalculatorDate").innerHTML = calcTimeDifference("2020-03-01");
+    document.getElementById("olxBotDate").innerHTML = calcTimeDifference("2020-03-04");
+    document.getElementById("desktopAlarmClockDate").innerHTML = calcTimeDifference("2020-03-10");
 }
 function calcTimeDifference(endDate) {
-    var nowDate = moment(); //todays date
-    var endDate = moment(endDate); // another date
-    var duration = moment.duration(nowDate.diff(endDate));
-    var diffYears = duration.get('years') > 0 ? duration.get('years') + " year(s) " : "";
-    var diffMonths = duration.get('months') > 0 ? duration.get('months') + " month(s) " : "";
-    var diffDays = duration.get('days') > 0 ? duration.get('days') + " day(s) " : "";
-    result = diffYears + diffMonths + diffDays;
-    console.log(result);
+    const nowDate = moment(); //todays date
+    let endParsedDate = moment(endDate); // another date
+    const duration = moment.duration(nowDate.diff(endParsedDate));
+    const diffYears = duration.get('years') > 0 ? duration.get('years') + " year(s) " : "";
+    const diffMonths = duration.get('months') > 0 ? duration.get('months') + " month(s) " : "";
+    const diffDays = duration.get('days') > 0 ? duration.get('days') + " day(s) " : "";
+    return diffYears + diffMonths + diffDays;
 }
